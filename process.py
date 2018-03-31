8652db48cf57069a4a61823ee8e6a1bb74f0dba3
Nihar Sheth <nsheth12@gmail.com>
2018-03-31 02:58:45 -0700

    make function

--- process.py
+++ process.py
@@ -1,25 +1,31 @@
 import cv2
 
-vidcap = cv2.VideoCapture('notebooks/rat_video.mp4')
-success, image = vidcap.read()
-count = 0
-success = True
 
-frames = []
-
-while success:
+def get_data(video_path):
+    vidcap = cv2.VideoCapture(video_path)
     success, image = vidcap.read()
-    print('Read a new frame: ', success)
-    if success:
-        frames.append(image)
-        # cv2.imwrite("frame%d.jpg" % count, image)
-        count += 1
+    count = 0
 
-x = []
-y = []
-for index, value in enumerate(frames):
-    if index < 2:
-        continue
+    frames = []
 
-    x.append([frames[index - 2], frames[index]])
-    y.append(frames[index - 1])
+    while success:
+        success, image = vidcap.read()
+        print('Read a new frame: ', success)
+        if success:
+            frames.append(image)
+            # cv2.imwrite("frame%d.jpg" % count, image)
+            count += 1
+
+    x = []
+    y = []
+    for index, value in enumerate(frames):
+        if index < 2:
+            continue
+
+        x.append([frames[index - 2], frames[index]])
+        y.append(frames[index - 1])
+
+    return x, y
+
+
+x, y = get_data("notebooks/rat_video.mp4")
