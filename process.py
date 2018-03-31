d961b38dc1986cf9605b9b0be08a298838b9982d
Nihar Sheth <nsheth12@gmail.com>
2018-03-31 02:36:02 -0700

    fixed process.py

--- process.py
+++ process.py
@@ -1,11 +1,24 @@
 import cv2
 
-vidcap = cv2.VideoCapture('notebooks/data/rat_video.mp4')
+vidcap = cv2.VideoCapture('notebooks/rat_video.mp4')
 success, image = vidcap.read()
 count = 0
 success = True
+
+frames = []
+
 while success:
     success, image = vidcap.read()
     print('Read a new frame: ', success)
-    cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+    frames.append(image)
+    # cv2.imwrite("frame%d.jpg" % count, image)
     count += 1
+
+x = []
+y = []
+for index, value in enumerate(frames):
+    if index < 2:
+        continue
+
+    x.append([frames[index - 2], frames[index]])
+    y.append(frames[index - 1])
