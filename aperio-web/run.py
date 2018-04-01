d5de74ccb45f36db2fc90a5e02a556b5ba9198df
Nihar Sheth <nsheth12@gmail.com>
2018-04-01 07:41:27 -0700

    add process file

--- aperio-web/run.py
+++ aperio-web/run.py
@@ -2,23 +2,22 @@ from flask import Flask, render_template, jsonify, request, send_file
 from random import *
 from flask_cors import CORS
 import requests
+from enhance_video import process_file
 
 app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+
 @app.route('/api/video', methods=['GET', 'POST'])
 def process_video():
     files = request.files
 
     for key, value in files.items():
-        print(key)
-        value.save("/tmp/input.jpeg")
+        value.save("/tmp/input.mp4")
+        process_file("/tmp/input.mp4")
 
-        #someones code
-
-        return send_file("/tmp/input.jpeg", mimetype="image/jpeg")
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
