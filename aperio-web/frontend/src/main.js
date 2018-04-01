651e5c5c3015a3bf51fdb5f799fc2fa6174b3228
Markie Wagner <markiewagner@Melissas-MacBook-Pro.local>
2018-04-01 01:47:09 -0700

    first commit

---
+++ aperio-web/frontend/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+import Vuetify from 'vuetify'
+// index.js or main.js
+import 'vuetify/dist/vuetify.min.css'
+import App from './App'
+import router from './router'
+
+Vue.use(Vuetify)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
